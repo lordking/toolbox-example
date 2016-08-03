542365b6e92fd1ad1975fd55271be2ba8bd8f32b
金磊 <lordking@163.com>
2016-08-03 14:28:57 +0800

    no message

--- database/redis/main.go
+++ database/redis/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"time"
+
 	"github.com/lordking/toolbox/common"
 	"github.com/lordking/toolbox/database"
 	"github.com/lordking/toolbox/database/redis"
-
 	"github.com/lordking/toolbox/log"
 )
 
@@ -12,23 +13,53 @@ func init() {
 	log.SetLevel(log.DebugLevel)
 }
 
+type MainDelegate struct{}
+
+func (d *MainDelegate) GetPerson(form *PersonForm) error {
+	log.Debug("form: %s", common.PrettyObject(form))
+
+	return nil
+}
+
 func main() {
 
-	redis := redis.New()
-	err := database.CreateInstance(redis, "./redis.json")
-	defer common.CheckFatal(err)
-
-	p := &Person{}
 	form := &PersonForm{
 		Name:  "leking",
 		Phone: "18900000000",
 	}
 
-	err = p.Set(form)
+	redis := redis.New()
+	err := database.CreateInstance(redis, "./redis.json")
 	defer common.CheckFatal(err)
 
-	f2, err := p.Get()
+	delegate := &MainDelegate{}
+
+	p := &Person{
+		Delegate: delegate,
+	}
+
+	//设置或新增
+	err = p.Set("leking", form, 10)
 	defer common.CheckFatal(err)
 
-	log.Debug("f2:%s", f2)
+	//获取
+	form, err = p.Get("leking")
+	defer common.CheckFatal(err)
+	log.Debug("form: %s", common.PrettyObject(form))
+
+	//删除
+	err = p.Delete("leking")
+	defer common.CheckFatal(err)
+
+	//订阅
+	err = p.Subscribe("person")
+	defer common.CheckFatal(err)
+
+	//发布
+	for {
+		err = p.Publish("person", form)
+		defer common.CheckFatal(err)
+		time.Sleep(1e9)
+	}
+
 }
--- database/redis/person.go
+++ database/redis/person.go
@@ -4,15 +4,18 @@ import (
 	"github.com/lordking/toolbox/common"
 	"github.com/lordking/toolbox/database"
 	"github.com/lordking/toolbox/database/redis"
-)
-
-const (
-	key = "person"
+	"github.com/lordking/toolbox/log"
 )
 
 //Person 用户数据对象
 type (
-	Person struct{}
+	PersonDelegate interface {
+		GetPerson(form *PersonForm) error
+	}
+
+	Person struct {
+		Delegate PersonDelegate
+	}
 
 	PersonForm struct {
 		Name  string `json:"name" bson:"name"`
@@ -20,7 +23,7 @@ type (
 	}
 )
 
-func (p *Person) Set(form *PersonForm) error {
+func (p *Person) Set(key string, form *PersonForm, expire int) error {
 
 	//获取单例
 	db := (database.Instance).(*redis.Redis)
@@ -29,7 +32,7 @@ func (p *Person) Set(form *PersonForm) error {
 		return common.NewErrorWithOther(common.ErrCodeInternal, err)
 	}
 
-	if err := db.SetObject(key, form); err != nil {
+	if err := db.SetObject(key, form, expire); err != nil {
 		return common.NewErrorWithOther(common.ErrCodeInternal, err)
 	}
 
@@ -40,7 +43,9 @@ func (p *Person) Set(form *PersonForm) error {
 	return nil
 }
 
-func (p *Person) Get() (*PersonForm, error) {
+func (p *Person) Get(key string) (*PersonForm, error) {
+
+	form := &PersonForm{}
 
 	//获取单例
 	db := (database.Instance).(*redis.Redis)
@@ -49,7 +54,6 @@ func (p *Person) Get() (*PersonForm, error) {
 		return nil, common.NewErrorWithOther(common.ErrCodeInternal, err)
 	}
 
-	form := &PersonForm{}
 	if err := db.GetObject(form, key); err != nil {
 		return nil, common.NewErrorWithOther(common.ErrCodeInternal, err)
 	}
@@ -59,4 +63,80 @@ func (p *Person) Get() (*PersonForm, error) {
 	}
 
 	return form, nil
+}
+
+func (p *Person) Delete(key string) error {
+
+	//获取单例
+	db := (database.Instance).(*redis.Redis)
+
+	if err := db.Connect(); err != nil {
+		return common.NewErrorWithOther(common.ErrCodeInternal, err)
+	}
+
+	if err := db.DeleteObject(key); err != nil {
+		return common.NewErrorWithOther(common.ErrCodeInternal, err)
+	}
+
+	if err := db.Close(); err != nil {
+		return common.NewErrorWithOther(common.ErrCodeInternal, err)
+	}
+
+	return nil
+}
+
+func (p *Person) Publish(channel string, form *PersonForm) error {
+
+	//获取单例
+	db := (database.Instance).(*redis.Redis)
+
+	if err := db.Connect(); err != nil {
+		return common.NewErrorWithOther(common.ErrCodeInternal, err)
+	}
+
+	if err := db.PublishObject(channel, form); err != nil {
+		return common.NewErrorWithOther(common.ErrCodeInternal, err)
+	}
+
+	if err := db.Close(); err != nil {
+		return common.NewErrorWithOther(common.ErrCodeInternal, err)
+	}
+
+	return nil
+}
+
+func (p *Person) Subscribe(channel string) error {
+
+	//获取单例
+	db := (database.Instance).(*redis.Redis)
+
+	if err := db.Connect(); err != nil {
+		return common.NewErrorWithOther(common.ErrCodeInternal, err)
+	}
+
+	psc, err := db.Subscribe(channel)
+	if err != nil {
+		return common.NewErrorWithOther(common.ErrCodeInternal, err)
+	}
+
+	db.Receive(psc)
+
+	go func() {
+
+		for {
+			data := <-db.ReceiveQueue
+
+			if p.Delegate != nil {
+				form := &PersonForm{}
+				common.ReadJSON(form, data)
+				if err := p.Delegate.GetPerson(form); err != nil {
+					log.Error("Receive error:", err)
+				}
+			}
+
+		}
+
+	}()
+
+	return nil
 }
